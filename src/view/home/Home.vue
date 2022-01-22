b7f2142d644bac0c3de8b25470e02ca62d782c22
ween <228574227@qq.com>
2022-01-22 15:06:25 +0800

    安装并配置tailwindCss

--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,13 +1,14 @@
 <template>
-  <h2>Home Page</h2>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <script lang="ts" setup>
-  import { useHomeStore } from '@/store/home'
-  const homeStore = useHomeStore()
-  const name = homeStore.name
-  console.log(name);
-
+import { useHomeStore } from '@/store/home'
+const homeStore = useHomeStore()
+const name = homeStore.name
+console.log(name)
 </script>
 
 <style lang="scss" scoped></style>
